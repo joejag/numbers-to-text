295220802b0a4969dff343b76c0953bc7a4f91db
Joe Wright <joe@joejag.com>
2012-09-06 23:53:55 +0100

    Fleshed out number breaker by using a padded version of the base number

--- src/main/java/com/joejag/numbertotext/breaker/NumberBreaker.java
+++ src/main/java/com/joejag/numbertotext/breaker/NumberBreaker.java
@@ -1,17 +1,38 @@
 package com.joejag.numbertotext.breaker;
 
+import com.joejag.numbertotext.utils.StringPadder;
+
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.joejag.numbertotext.breaker.NumberComponent.NumberComponentPart.*;
 
 public class NumberBreaker {
 
     public NumberComponents breakDown(int baseNumber) {
         List<NumberComponent> components = new ArrayList<NumberComponent>();
 
-        if (baseNumber < 1000)
-            components.add(new NumberComponent(NumberComponent.NumberComponentPart.HUNDREDS, baseNumber));
+        String parts = createdAPaddedRepresentationOfTheBaseNumber(baseNumber);
+
+        addPartIfHasValue(components, parts, 9, 6, MILLIONS);
+        addPartIfHasValue(components, parts, 6, 3, THOUSANDS);
+        addPartIfHasValue(components, parts, 3, 0, HUNDREDS);
 
         return new NumberComponents(components);
+    }
+
+    private void addPartIfHasValue(List<NumberComponent> components, String parts,
+                                   int negativeStart, int negativeFinish, NumberComponent.NumberComponentPart part) {
+        int partialNumber = grabPartOfNumber(parts, negativeStart, negativeFinish);
+        if (partialNumber != 0)
+            components.add(new NumberComponent(part, partialNumber));
+    }
+
+    private Integer grabPartOfNumber(String parts, int negativeStart, int negativeFinish) {
+        return Integer.valueOf(parts.substring(parts.length() - negativeStart, parts.length() - negativeFinish));
+    }
+
+    private String createdAPaddedRepresentationOfTheBaseNumber(int baseNumber) {
+        return StringPadder.padLeft(String.valueOf(baseNumber), "0", 9);
     }
 }
--- src/test/java/com/joejag/numbertotext/breaker/NumberBreakerTest.java
+++ src/test/java/com/joejag/numbertotext/breaker/NumberBreakerTest.java
@@ -2,7 +2,7 @@ package com.joejag.numbertotext.breaker;
 
 import org.junit.Test;
 
-import static com.joejag.numbertotext.breaker.NumberComponent.NumberComponentPart.HUNDREDS;
+import static com.joejag.numbertotext.breaker.NumberComponent.NumberComponentPart.*;
 import static junit.framework.Assert.assertEquals;
 
 public class NumberBreakerTest {
@@ -11,7 +11,31 @@ public class NumberBreakerTest {
     public void number_in_the_hundreds_returns_just_itself() {
         NumberComponents components = new NumberBreaker().breakDown(100);
         assertEquals(1, components.components.size());
-        assertEquals(100, components.components.get(0).number);
-        assertEquals(HUNDREDS, components.components.get(0).part);
+
+        assertPart(components.components.get(0), 100, HUNDREDS);
+    }
+
+    @Test
+    public void number_in_the_thousands_returns_two_parts() {
+        NumberComponents components = new NumberBreaker().breakDown(1100);
+        assertEquals(2, components.components.size());
+
+        assertPart(components.components.get(0), 1, THOUSANDS);
+        assertPart(components.components.get(1), 100, HUNDREDS);
+    }
+
+    @Test
+    public void number_in_the_millions_returns_three_parts() {
+        NumberComponents components = new NumberBreaker().breakDown(100100100);
+        assertEquals(3, components.components.size());
+
+        assertPart(components.components.get(0), 100, MILLIONS);
+        assertPart(components.components.get(1), 100, THOUSANDS);
+        assertPart(components.components.get(2), 100, HUNDREDS);
+    }
+
+    private void assertPart(NumberComponent component, int expectedNumber, NumberComponent.NumberComponentPart expectedPart) {
+        assertEquals(expectedNumber, component.number);
+        assertEquals(expectedPart, component.part);
     }
 }
