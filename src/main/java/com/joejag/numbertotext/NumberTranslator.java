2ade25e4ab5cec7e6626b6f65b5155009ee3bab6
Joe Wright <joe@joejag.com>
2012-09-06 18:33:48 +0100

    Added 3..20 where there's no need for an algorithm

--- src/main/java/com/joejag/numbertotext/NumberTranslator.java
+++ src/main/java/com/joejag/numbertotext/NumberTranslator.java
@@ -8,6 +8,24 @@ public class NumberTranslator {
     private Map<Integer, String> britishEnglish = new HashMap<Integer, String>(){{
         put(1, "one");
         put(2, "two");
+        put(3, "three");
+        put(4, "four");
+        put(5, "five");
+        put(6, "six");
+        put(7, "seven");
+        put(8, "eight");
+        put(9, "nine");
+        put(10, "ten");
+        put(11, "eleven");
+        put(12, "twelve");
+        put(13, "thirteen");
+        put(14, "fourteen");
+        put(15, "fifteen");
+        put(16, "sixteen");
+        put(17, "seventeen");
+        put(18, "eighteen");
+        put(19, "nineteen");
+        put(20, "twenty");
     }};
 
     public String translate(int number) {
--- src/test/java/com/joejag/numbertotext/NumberTranslatorTest.java
+++ src/test/java/com/joejag/numbertotext/NumberTranslatorTest.java
@@ -13,7 +13,37 @@ public class NumberTranslatorTest {
 
     @Test
     public void translate_2_to_two() {
-        assertEquals("two", new NumberTranslator().translate(2));
+        assertConversion("two", 2);
+    }
+
+    @Test
+    public void translate_3_to_numbers_below_10() {
+        assertConversion("three", 3);
+        assertConversion("four", 4);
+        assertConversion("five", 5);
+        assertConversion("six", 6);
+        assertConversion("seven", 7);
+        assertConversion("eight", 8);
+        assertConversion("nine", 9);
+    }
+
+    @Test
+    public void translate_10_to_20() {
+        assertConversion("ten", 10);
+        assertConversion("eleven", 11);
+        assertConversion("twelve", 12);
+        assertConversion("thirteen", 13);
+        assertConversion("fourteen", 14);
+        assertConversion("fifteen", 15);
+        assertConversion("sixteen", 16);
+        assertConversion("seventeen", 17);
+        assertConversion("eighteen", 18);
+        assertConversion("nineteen", 19);
+        assertConversion("twenty", 20);
+    }
+
+    private void assertConversion(String expected, int input) {
+        assertEquals(expected, new NumberTranslator().translate(input));
     }
 
 
